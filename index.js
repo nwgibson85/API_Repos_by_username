97dee8a72a42fd890f10404902de812e6c518cad
Nicholas Gibson <Nwgibson85@gmail.com>
2019-06-20 09:02:30 -0500

    finished

--- index.js
+++ index.js
@@ -1,39 +1,52 @@
 'use strict';
 
-const rootUrl = 'https://api.github.com/users/${userName}/repos';
+let rootUrl;
 
-// const apiKey;
+const apiKey = "a06fce7623cf5eb14abe05e3eb9b2b2168230d6c"
 
 function sendGetRequest() {
     let userName = $('#js-userName-search').val();
-    console.log(${userName});
-    fetch(`https://api.github.com/users/${userName}/repos`)
+    console.log(userName);
+    rootUrl = `https://cors-anywhere.herokuapp.com/https://api.github.com/users/${userName}/repos`
+    
+    const options = {
+        headers: new Headers({
+          "X-Api-Key": apiKey})
+    };
+    
+    fetch(rootUrl, options)
     .then(response => {
       if (response.ok) {
         return response.json();
       }
       throw new Error(response.statusText);
     })
-    .then(responseJson => displayRepos(responseJson))
+    .then(responseJson => responseJson.forEach(repo => displayRepos(repo)))
     .catch(err => {
-      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+      $('#js-error-message').text(`Something went wrong: ${err.message}, Please try another username.`);
     });
 }
 
 function displayRepos(responseJson) {
    console.log(responseJson);
-  $('#results-list').empty();
+  
   $('#results-list').append(
-    `<li><h3><a href="${responseJson.html_url}">${responseJson.full_Name}</a></h3>
-    <p>${responseJson.name}</p>
-    <p>${responseJson.description}</p>
+    `<li><h3><a href="${responseJson.html_url}">Link:${responseJson.full_name}</a></h3>
+    <p>Repo name: ${responseJson.name}</p>
+    <p>description: ${responseJson.description}</p>
     </li>`);
     $('#results').removeClass('hidden');
+    $('#clear').removeClass('hidden');
+}
+
+function clearResults() {
+    $('#results-list').empty();
 }
 
 function watchForm() {
-    $('form').submit(event => {
+    $('#js-form').submit(event => {
       event.preventDefault();
+      clearResults();
       sendGetRequest();
     });
   }
